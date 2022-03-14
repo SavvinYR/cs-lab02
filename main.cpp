d338df8e3bb95affa42cf286fec4d351ccf20cad
Bob <bob@example/com>
2022-03-14 16:43:37 +0300

    build: вывод произведения

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 int main()
 {
-    {int a, b;
+    int a, b;
    cout << "Enter A and B: ";
    cin>> a >> b;
-cout << "A + B = " << a + b << '\n'
+cout << "A + B = " << a + b << '\n'<< "A - B = "<<a - b<< '\n' << "A * B = " << a * b << '\n';
 }
